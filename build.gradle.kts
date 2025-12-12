5bcae4e2ef525e1ebe4041c59f374d3c4b2147c1
khatejang <aguarinmarjorie@gmail.com>
2025-12-12 09:21:53 +0800

    edited the applicationId

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,13 +5,13 @@ plugins {
 }
 
 android {
-    namespace = "com.example.tempfixproject"
+    namespace = "com.jltcb.jtlcbapp"
     compileSdk {
         version = release(36)
     }
 
     defaultConfig {
-        applicationId = "com.example.tempfixproject"
+        applicationId = "com.jltcb.jtlcbapp"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
