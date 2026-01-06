429acb40d578da61fa51f74af4c7918e0e613d50
Khatejang <aguarinmarjorie@gmail.com>
2026-01-06 17:37:03 +0800

    🚧 ports catered_in-progressed

--- app/(pages)/ports-catered/Ports.tsx
+++ app/(pages)/ports-catered/Ports.tsx
@@ -1,9 +1,14 @@
-import {Text} from "react-native"
+import {Text, View} from "react-native"
+import { Button } from "react-native-paper"
 
 export default function Ports(){
     return(
         <>
-        <Text>Hello</Text>
+        <View style={{flexDirection:"row"}}>
+        <Button mode="outlined" style={{ borderRadius: 0 }} rippleColor="#EE9034">Hello</Button>
+        <Button>Hello</Button>
+        <Button>Hello</Button>
+        </View>
         </>
     )
 }
