9e1b94b2026eae1262fb2c0dd18e8aa4eb7401ed
rndall <96964503+rndall@users.noreply.github.com>
2026-01-14 14:40:41 +0800

    :wrench: Implement tariff schedule selection checkboxes

---
+++ src/constants/tariffSchedules.ts
@@ -0,0 +1,61 @@
+export default [
+  {
+    label: "ASEAN-Australia-New Zealand Free Trade Agreement",
+    value: "AANZFTA",
+  },
+  {
+    label: "ASEAN-China Free Trade Agreement",
+    value: "ACFTAF",
+  },
+  {
+    label: "ASEAN-Hong Kong, China Free Trade Agreement",
+    value: "AHKFTA",
+  },
+  {
+    label: "ASEAN-India Free Trade Agreement",
+    value: "AIFTA",
+  },
+  {
+    label: "ASEAN-Japan Comprehensive Economic Partnership Agreement",
+    value: "AJCEPA",
+  },
+  {
+    label: "ASEAN-Korea Free Trade Agreement",
+    value: "AAKFTA",
+  },
+  {
+    label: "ASEAN Trade in Goods Agreement",
+    value: "ATIGA",
+  },
+  {
+    label: "Most Favoured Nation",
+    value: "MFN",
+  },
+  {
+    label:
+      "Philippines-European Free Trade Association Free Trade Agreement (Switzerland/Liechtenstein)",
+    value: "PH-EFTA FTA (CHE/LIE)",
+  },
+  {
+    label:
+      "Philippines-European Free Trade Association Free Trade Agreement (Iceland)",
+    value: "PH-EFTA FTA (ISL)",
+  },
+  {
+    label:
+      "Philippines-European Free Trade Association Free Trade Agreement (Norway)",
+    value: "H-EFTA FTA (NOR)",
+  },
+  {
+    label: "Philippines-Korea Free Trade Agreement",
+    value: "PH-KR FTA",
+  },
+  {
+    label: "Philippines-Japan Economic Partnership Agreement",
+    value: "PJEPA",
+  },
+  {
+    label: "Regional Comprehensive Economic Partnership Agreement",
+    value: "RCEP",
+  },
+];
